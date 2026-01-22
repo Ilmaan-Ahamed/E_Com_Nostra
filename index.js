fd235cd1905c29c17d1247f0820e5e4e280737f4
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-01-23 00:55:26 +0530

    Js funtion for shop now btn

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ function closeNavbar()
         sidenav.style.left="-60%"
     }
 
+document.querySelectorAll(".new-arrival button")
+        .forEach(function (buy){
+                buy.addEventListener("click", function(){
+                    window.location.href = "buy.html";
+                                    });
+                                });
 
+
+
+// document.getElementById("buy").onbeforematch(function ())
+//         .addEventListener("click", function(){
+//         window.location.href = "buy.html";
+//         })
